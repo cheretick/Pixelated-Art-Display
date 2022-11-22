87a97c79f44ded9736d9efd61c1c62e2350d0ca4
Collin Heretick <collinheretick@gmail.com>
2022-11-22 16:56:57 -0500

    changed some parameters, set up for recording example video

--- pixelate4.py
+++ pixelate4.py
@@ -2,17 +2,15 @@ import cv2 as cv
 import pickle
 import numpy as np
 
-PIXEL_SIZE = 15
+PIXEL_SIZE = 10
 # set width and height to 'None' to automatically set resolution
 # the webcam driver will likely select the closest available resolution when using a custom value
-width = None
-height = None
+width = 1280
+height = 720
 capture = cv.VideoCapture(1)
-foregroundFeed = cv.createBackgroundSubtractorMOG2(history=250, varThreshold= 32, detectShadows=False) # defaults: 500, 16, True
+foregroundFeed = cv.createBackgroundSubtractorMOG2(history=500, varThreshold= 60, detectShadows=False) # defaults: 500, 16, True
 
-video = cv.VideoCapture("videos\girl2.mp4")
-
-
+video = cv.VideoCapture("G:\\School\\2022 FALL\\ENGT 4050\\videos\\edited\\test.mp4")
 
 if width == None or height == None:
     # opens resolutions.data and adds them to resolutions list (up to 1080p)
@@ -63,19 +61,19 @@ cv.namedWindow("Pixelate", cv.WINDOW_NORMAL)
 #low = np.array([0,0,0])
 #high = np.array([25,25,25])
 grey = np.zeros(background.shape, np.uint8)
-grey.fill(20)
+grey.fill(25)
 
-#out = cv.VideoWriter('videos\girlExample2.mp4', cv.VideoWriter_fourcc(*'MP4V'), 29.97, (int(width),int(height)))
+out = cv.VideoWriter('videos\example.mp4', cv.VideoWriter_fourcc(*'MP4V'), 29.97, (int(width),int(height)))
 
 while(True):
     isTrue, frame = capture.read()
-    #isTrue, videoCap = video.read()  # uses video as input
+    isTrue, videoCap = video.read()  # uses video as input
 
-    #videoCap = cv.resize(videoCap,(int(width),int(height)),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
-    #frame = videoCap #inserts video into frame instead of webcam
+    videoCap = cv.resize(videoCap,(int(width),int(height)),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
+    frame = videoCap #inserts video into frame instead of webcam
 
-    frame = foregroundFeed.apply(frame)
-    mask = cv.inRange(frame, 0, 125)
+    #frame = foregroundFeed.apply(frame)  # apply the background removal algorithm
+    mask = cv.inRange(frame, (0,0,0), (245,245,245))
     mask = pixelate(PIXEL_SIZE, mask)
     invMask = cv.bitwise_not(mask)
     #frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR) #matches color channel for bitwise_and
@@ -84,9 +82,13 @@ while(True):
     # pixelates image
     #frame = pixelate(PIXEL_SIZE, frame)
 
-    foregroundOnly = cv.bitwise_and(grey, grey, mask = invMask)
-    backgroundOnly = cv.bitwise_and(background, background, mask = mask)
+    # normal mask layers
+    #foregroundOnly = cv.bitwise_and(grey, grey, mask = invMask)
+    #backgroundOnly = cv.bitwise_and(background, background, mask = mask)
 
+    # reverse mask layers
+    foregroundOnly = cv.bitwise_and(grey, grey, mask = mask)
+    backgroundOnly = cv.bitwise_and(background, background, mask = invMask)
 
     #camWithBG = cv.bitwise_and(background, background, mask=mask)
     camWithBG = cv.add(foregroundOnly, backgroundOnly)
@@ -101,12 +103,12 @@ while(True):
     #cv.imshow('foreground', foregroundOnly)
     #cv.imshow('background', backgroundOnly)
 
-    #out.write(camWithBG)
+    out.write(camWithBG)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 print("done!")
 capture.release()
-#out.release()
+out.release()
 cv.destroyAllWindows()
