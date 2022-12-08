723677ba15c92fe3ba456f4d4787ed8c2b5f980e
Collin Heretick <collinheretick@gmail.com>
2022-12-07 20:38:27 -0500

    updated Jetson compatible version to include plaque

--- pixelateFinalJetson.py
+++ pixelateFinalJetson.py
@@ -9,18 +9,18 @@ videoNumber = 1
 previousVideo = videoNumber
 width = 640
 height = 360
-counter = random.randint(100,1000)
+counter = random.randint(100,1300)
 if DEMO_MODE:
     counter = 0
 capture = cv.VideoCapture("/dev/video0", cv.CAP_V4L2)
-foregroundFeed = cv.createBackgroundSubtractorMOG2(history=500, varThreshold= 45, detectShadows=False) # defaults: 500, 16, True
+foregroundFeed = cv.createBackgroundSubtractorMOG2(history=500, varThreshold= 35, detectShadows=False) # defaults: 500, 16, True
 video = cv.VideoCapture("/home/collin/Pixelated-Art-Display/videos/" + str(videoNumber) + ".mp4")
 
 # set manual webcam dimensions
 capture.set(cv.CAP_PROP_FRAME_WIDTH, width)
 capture.set(cv.CAP_PROP_FRAME_HEIGHT, height)
     
-# get webcam dimensions
+# get webcam dimensions (in case webcam driver does not support chosen resolution)
 width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
 height = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
 
@@ -50,18 +50,29 @@ grey.fill(25)
 # morphology fill size
 kernel = np.ones((PIXEL_SIZE, PIXEL_SIZE), np.uint8)
 
+# for plaque
+plaque = cv.imread("/home/collin/Pixelated-Art-Display/backgroundWithPlaque.jpg")
+plaque = cv.resize(plaque, (int(width), int(height)))
+plaqueCounter = 1
+
 while(True):
     if (counter > 0): # use webcam for input
         retCam, frame = capture.read()
         frame = foregroundFeed.apply(frame)  # apply the background removal algorithm
         mask = cv.inRange(frame, 0, 125)
-	if UPSIDEDOWN_MODE:
+        if UPSIDEDOWN_MODE:
             mask = cv.flip(mask, 0)
         mask = pixelate(PIXEL_SIZE, mask)
         invMask = cv.bitwise_not(mask)
         foregroundOnly = cv.bitwise_and(grey, grey, mask = invMask)
         backgroundOnly = cv.bitwise_and(background, background, mask = mask)
         counter -= 1
+        if counter == 50:
+            if (random.randint(1,5) == 1): 
+                background = plaque
+            else:
+                background = cv.imread('/home/collin/Pixelated-Art-Display/generatedImage.jpg')
+                background = cv.resize(background, (int(width), int(height)))
     else: # use prerecorded video as input
         retVideo, frame = video.read()
         if retVideo: 
@@ -78,14 +89,22 @@ while(True):
             previousVideo = videoNumber
             video = cv.VideoCapture("/home/collin/Pixelated-Art-Display/videos/" + str(videoNumber) + ".mp4")
             if not DEMO_MODE:
-                counter = random.randint(100,1000)
+                counter = random.randint(100,1300)
+            if DEMO_MODE:
+                if (random.randint(1,6) == 1) or (plaqueCounter == 0): 
+                    background = plaque
+                    plaqueCounter -= 1
+                else:
+                    background = cv.imread('/home/collin/Pixelated-Art-Display/generatedImage.jpg')
+                    background = cv.resize(background, (int(width), int(height)))
+                    plaqueCounter = 1
 
     # combines foreground and background
     camWithBG = cv.add(foregroundOnly, backgroundOnly)
 
     # mirrors the frame
     if not UPSIDEDOWN_MODE:
-	 camWithBG = cv.flip(camWithBG, 1)
+        camWithBG = cv.flip(camWithBG, 1)
 
     cv.imshow('Pixelate', camWithBG)
 
